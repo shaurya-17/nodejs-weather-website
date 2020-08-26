68795da39b17ce3b09e99714daf4deae0f771f4f
Shaurya Bhatnagar <shaurya.bhatnagar@lvertical.com>
2020-08-26 08:49:09 +0530

    setupfotheroku

--- public/js/app.js
+++ public/js/app.js
@@ -38,7 +38,7 @@ messageTwo.textContent=''
 
 const location = search.value
 
-fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             console.log(data.error)
